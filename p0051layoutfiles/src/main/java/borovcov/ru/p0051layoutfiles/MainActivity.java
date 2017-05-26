c7821462d747af2e6b6461c392e978e2e3528f18
Vlad Borovtsov <borovcov.vlad@gmail.com>
2017-05-26 19:46:27 +0700

    lesson 5 done

--- p0051layoutfiles/src/main/java/borovcov/ru/p0051layoutfiles/MainActivity.java
+++ p0051layoutfiles/src/main/java/borovcov/ru/p0051layoutfiles/MainActivity.java
@@ -2,12 +2,16 @@ package borovcov.ru.p0051layoutfiles;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.myscreen);
     }
+
+
 }
